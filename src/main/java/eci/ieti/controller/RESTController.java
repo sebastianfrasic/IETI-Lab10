19d002dc7aafdae90c0c9e74f4c8d365b3903abb
Juan Sebastián Frásica G <juansebastianfrasica@hotmail.com>
2021-04-15 19:42:30 -0500

    adding front

--- src/main/java/eci/ieti/controller/RESTController.java
+++ src/main/java/eci/ieti/controller/RESTController.java
@@ -1,60 +1,66 @@
 package eci.ieti.controller;
 
 
+import com.mongodb.client.gridfs.model.GridFSFile;
+import eci.ieti.data.TodoRepository;
 import eci.ieti.data.model.Todo;
-import eci.ieti.services.FileServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.gridfs.GridFsResource;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
 @RequestMapping("api")
 @RestController
 public class RESTController {
+    @Autowired
+    GridFsTemplate gridFsTemplate;
 
     @Autowired
-    private FileServices fileServices;
-
+    TodoRepository todoRepository;
 
     @RequestMapping("/files/{filename}")
-    public ResponseEntity<InputStreamResource> getFileByName(@PathVariable String filename) {
-        InputStreamResource file;
-        try {
-            file = fileServices.getFileByName(filename);
-        } catch (IOException e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    public ResponseEntity<InputStreamResource> getFileByName(@PathVariable String filename) throws IOException {
+
+        GridFSFile file = gridFsTemplate.findOne(new Query().addCriteria(Criteria.where("filename").is(filename)));
+        if(file != null){
+            GridFsResource resource = gridFsTemplate.getResource(file.getFilename());
+            return ResponseEntity.ok()
+                    .contentType(MediaType.valueOf(resource.getContentType()))
+                    .body(new InputStreamResource(resource.getInputStream()));
         }
-        return new ResponseEntity<>(file, HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 
     }
 
     @CrossOrigin("*")
     @PostMapping("/files")
     public String handleFileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) throws IOException {
-
-        //TODO implement method
-        return null;
+        gridFsTemplate.store(file.getInputStream(), file.getOriginalFilename(), file.getContentType());
+        return ("http://localhost:8080/api/files/"+file.getOriginalFilename());
     }
 
     @CrossOrigin("*")
     @PostMapping("/todo")
     public Todo createTodo(@RequestBody Todo todo) {
-        //TODO implement method
-        return null;
+        todoRepository.save(todo);
+        return todo;
     }
 
     @CrossOrigin("*")
     @GetMapping("/todo")
     public List<Todo> getTodoList() {
-        //TODO implement method
-        return null;
+        return todoRepository.findAll();
     }
 
 }
